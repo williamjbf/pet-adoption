fbc2b4010bd1ca50febbd48eeef34ab70fb6aa86
williamjbf <williamjbf@gmail.com>
2022-06-02 23:39:54 -0300

    added animal list styling

--- frontend/src/ui/components/List/List.tsx
+++ frontend/src/ui/components/List/List.tsx
@@ -13,8 +13,16 @@ export default function List(){
                 <Photo src={'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQq3cRj6OTuIS3F66-rs1I55ayBShO9ojDJ8K1jCzg5jo-LFlfgCnl4ZNl-SEu5nuOFr1Uizy7qM2trmHDSwsJPieWIkmT_yah44pxTqKJ0BZLgMSIr-pzn&usqp=CAE'}></Photo>
                 <Info>
                     <Name>Rex</Name>
-                    <Description>lorem</Description>
-                    <Button variant={'contained'}>Adotar</Button>
+                    <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque vestibulum dignissim. Quisque ornare egestas gravida. Integer quis ex vulputate, mollis velit ac, condimentum turpis. Nam suscipit sollicitudin lacinia. Fusce sit amet turpis lectus. Sed non libero id metus ornare vulputate eget eget urna. Proin sit amet aliquet ipsum. Proin eu lorem laoreet, pharetra tellus sed, volutpat nisl. Proin consectetur vitae augue sit amet suscipit. Pellentesque venenatis libero justo, nec ornare sapien interdum sed.</Description>
+                    <Button variant={'contained'} fullWidth>Adotar</Button>
+                </Info>
+            </ItemList>
+            <ItemList>
+                <Photo src={'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQq3cRj6OTuIS3F66-rs1I55ayBShO9ojDJ8K1jCzg5jo-LFlfgCnl4ZNl-SEu5nuOFr1Uizy7qM2trmHDSwsJPieWIkmT_yah44pxTqKJ0BZLgMSIr-pzn&usqp=CAE'}></Photo>
+                <Info>
+                    <Name>Rex</Name>
+                    <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque vestibulum dignissim. Quisque ornare egestas gravida. Integer quis ex vulputate, mollis velit ac, condimentum turpis. Nam suscipit sollicitudin lacinia. Fusce sit amet turpis lectus. Sed non libero id metus ornare vulputate eget eget urna. Proin sit amet aliquet ipsum. Proin eu lorem laoreet, pharetra tellus sed, volutpat nisl. Proin consectetur vitae augue sit amet suscipit. Pellentesque venenatis libero justo, nec ornare sapien interdum sed.</Description>
+                    <Button variant={'contained'} fullWidth>Adotar</Button>
                 </Info>
             </ItemList>
         </ListStyled>
